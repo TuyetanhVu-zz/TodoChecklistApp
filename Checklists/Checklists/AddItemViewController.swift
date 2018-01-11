4976e49ae3ec12288d71b77432d9598e7c1c34fb
Tuyet <tuyet22.vu@gmail.com>
2018-01-10 19:41:27 -0500

    adding/deleting items
    created rows for checklist when tapped on add button, created an array for generic string titles, added a function to delete rows by swiping, added another table view controller for add button, added navigation item and cancel/done bar button item, added a class file for add item view controller, changed table view content to static cells, added a text field to add item view, and created function to replace old text with new text.

---
+++ Checklists/Checklists/AddItemViewController.swift
@@ -0,0 +1,54 @@
+//
+//  AddItemViewController.swift
+//  Checklists
+//
+//  Created by Tuyetanh Vu on 1/10/18.
+//  Copyright © 2018 Tuyetanh Vu. All rights reserved.
+//
+
+import UIKit
+
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
+    @IBOutlet weak var cancelBarButton: UIBarButtonItem!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.largeTitleDisplayMode = .never
+
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        textField.becomeFirstResponder()
+    }
+    
+    @IBAction func cancel(){
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func done(){
+        navigationController?.popViewController(animated: true)
+        print("Contents of text field: \(textField.text!)")
+    }
+    
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        return nil
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let oldText = textField.text!
+        let stringRange = Range(range, in:oldText)
+        let newText = oldText.replacingCharacters(in: stringRange!, with: string)
+        if newText.isEmpty{
+            doneBarButton.isEnabled = false
+            
+        } else {
+            doneBarButton.isEnabled = true
+        }
+        return true
+    }
+    
+}
--- Checklists/Checklists/ChecklistViewController.swift
+++ Checklists/Checklists/ChecklistViewController.swift
@@ -13,7 +13,23 @@ class ChecklistViewController: UITableViewController {
     var items: [ChecklistItem]
 
     @IBAction func addItem(_ sender: Any) {
-        print("added item")
+        let newRowIndex = items.count
+        
+        let item = ChecklistItem()
+        //item.text = "I am a new row"
+        var titles = ["Empty todo item", "Generic todo item", "First todo: fill me out", "I need something todo",
+                      "Much todo about nothing"]
+        let randomNumber = arc4random_uniform(UInt32(titles.count))
+        let title = titles[Int(randomNumber)]
+        item.text = title
+        
+        item.checked = true
+        
+        items.append(item)
+        
+        let indexPath = IndexPath(row: newRowIndex, section: 0)
+        let indexPaths = [indexPath]
+        tableView.insertRows(at: indexPaths, with: .automatic)
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -30,19 +46,16 @@ class ChecklistViewController: UITableViewController {
         row1Item.checked = false
         items.append(row1Item)
 
-        
         let row2Item = ChecklistItem()
         row2Item.text  = "Learn iOS development"
         row2Item.checked = false
         items.append(row2Item)
 
-        
         let row3Item = ChecklistItem()
         row3Item.text  = "Soccer practice"
         row3Item.checked = false
         items.append(row3Item)
 
-        
         let row4Item = ChecklistItem()
         row4Item.text  = "Eat ice cream"
         row4Item.checked = false
@@ -53,24 +66,19 @@ class ChecklistViewController: UITableViewController {
         row5Item.checked = true
         items.append(row5Item)
         
-        
         let row6Item = ChecklistItem()
         row6Item.text  = "Read iOS Apprentice"
         row6Item.checked = true
         items.append(row6Item)
-        
         
         let row7Item = ChecklistItem()
         row7Item.text  = "Make coffe"
         row7Item.checked = false
         items.append(row7Item)
 
-        
         super .init(coder: aDecoder)
     }
     
-   
-
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -82,6 +90,17 @@ class ChecklistViewController: UITableViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+        items.remove(at: indexPath.row)
+        //let indexPaths = [indexPath]
+        //tableView.deleteRows(at: indexPaths, with: .automatic)
+        
+        tableView.reloadData()
+    }
+    
+    
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return items.count
